90b9d29a01d407a31a5af17c01809cdb547aa4d0
Alenalena52 <118267598+Alenalena52@users.noreply.github.com>
2023-04-17 14:05:53 +0300

    Update test_show_my_pets.py

--- tests/test_show_my_pets.py
+++ tests/test_show_my_pets.py
@@ -9,18 +9,22 @@ def test_show_my_pets():
 
    # Устанавливаем явное ожидание
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, "email")))
+   
    # Вводим email
    pytest.driver.find_element_by_id('email').send_keys(valid_email)
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, "pass")))
+   
    # Вводим пароль
    pytest.driver.find_element_by_id('pass').send_keys(valid_password)
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit']")))
+   
    # Нажимаем на кнопку входа в аккаунт
    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Мои питомцы")))
+   
    # Нажимаем на ссылку "Мои питомцы"
    pytest.driver.find_element_by_link_text("Мои питомцы").click()
 
